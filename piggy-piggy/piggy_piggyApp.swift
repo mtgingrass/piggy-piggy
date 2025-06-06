a6ab86dedde2b521c31ac24bcf9cc16d7cc92326
mtgingrass <mark.gingrass@gmail.com>
2025-06-06 17:06:23 +0200

    Initial commit

---
+++ piggy-piggy/piggy_piggyApp.swift
@@ -0,0 +1,17 @@
+//
+//  piggy_piggyApp.swift
+//  piggy-piggy
+//
+//  Created by Mark Gingrass on 6/6/25.
+//
+
+import SwiftUI
+
+@main
+struct piggy_piggyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ piggy-piggyTests/piggy_piggyTests.swift
@@ -0,0 +1,17 @@
+//
+//  piggy_piggyTests.swift
+//  piggy-piggyTests
+//
+//  Created by Mark Gingrass on 6/6/25.
+//
+
+import Testing
+@testable import piggy_piggy
+
+struct piggy_piggyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
